f31d7effc9c96860befbfd616eabe93637072cd9
dev-back55 <ahabitu@gmail.com>
2022-11-21 10:46:44 -0300

    Feact/add tables octavos cuartos

---
+++ src/models/Cuartos.js
@@ -0,0 +1,53 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../database/db.js";
+
+const Cuartos = sequelize.define(
+  "cuartos",
+  {
+    dia: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    horario: {
+      type: DataTypes.STRING,
+    },
+    equipo_a: {
+      type: DataTypes.STRING,
+    },
+    equipo_b: {
+      type: DataTypes.STRING,
+    },
+    estadio: {
+      type: DataTypes.STRING,
+    },
+    clave_a: {
+      type: DataTypes.STRING,
+    },
+    clave_b: {
+      type: DataTypes.STRING,
+    },
+    ganador: {
+      type: DataTypes.STRING,
+    },
+    goles_a: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    goles_b: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+    flag_a: {
+      type: DataTypes.STRING,
+    },
+    flag_b: {
+      type: DataTypes.STRING,
+    },
+    canaltv: {
+      type: DataTypes.STRING,
+    },
+  },
+  { timestamps: false }
+);
+
+export default Cuartos;
